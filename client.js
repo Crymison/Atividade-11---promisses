858e40e63d93b57940d3e3f51327195f61a79765
Crymison <gabrielpaleta@hotmail.com>
2021-11-24 16:48:22 -0300

    Commit exercicio 1.2

--- client.js
+++ client.js
@@ -5,17 +5,27 @@ axios.post('http://localhost:3000/books', {
     name: 'My Boook',
     author: 'Me and only me'
 })
-axios.post('http://localhost:3000/books', {
-    ID: 4,
-    name: 'Prince of Thorns',
-    author: 'Mark Lawrence'
-})
-axios.post('http://localhost:3000/books', {
-    ID: 4,
-    name: 'Prince of Thorns 2',
-    author: 'Mark Lawrence'
-})
     .then((res) => {
+        console.log(res.data);
+    })
+    .catch(err => {
+        console.log(err.response.data);
+    });
+    axios.post('http://localhost:3000/books', {
+        ID: 4,
+        name: 'Prince of Thorns',
+        author: 'Mark Lawrence'
+    }).then((res) => {
+        console.log(res.data);
+    })
+    .catch(err => {
+        console.log(err.response.data);
+    });
+    axios.post('http://localhost:3000/books', {
+        ID: 4,
+        name: 'Prince of Thorns 2',
+        author: 'Mark Lawrence'
+    }).then((res) => {
         console.log(res.data);
     })
     .catch(err => {
@@ -23,7 +33,7 @@ axios.post('http://localhost:3000/books', {
     });
 
 
-axios.get('http://localhost:3000/books')
+    axios.get('http://localhost:3000/books')
     .then((response) => {
         console.log(response.data);
     });
